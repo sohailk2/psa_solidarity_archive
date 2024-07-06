e16c3541bd60611e92ad2c46ef6a73c6b9707113
Sohail Khan <sohailk2@illinois.edu>
2024-07-05 20:31:44 -0500

    API Url Update

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,8 +1,8 @@
 import Gallery from "./gallery";
 
 
-export default async function Archive() {
-    const response = await fetch('http://localhost:3000/api/archive/')
+export default async function Archive() {    
+    const response = await fetch('https://psa-solidarity-archive.vercel.app/api/archive/')
     const data = await response.json()
     console.log("Works", data.data)
 
