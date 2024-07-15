1a98ce303a238eaf5012339f5ea3b6a0dc8d2ecc
Syeda Tabassum <syedatabassum93@gmail.com>
2024-07-15 16:21:36 -0400

    styling update

--- src/app/components/navigation/navbar.tsx
+++ src/app/components/navigation/navbar.tsx
@@ -1,22 +1,28 @@
-import { Button } from "@/components/ui/button"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 
 export default function NavBar() {
-    return (
-        <div className="flex pt-24 px-4 sm:px-10 md:px-20 lg:px-40 xl:px-80 justify-center">
-            {/* <Button className="mx-1 light">Home</Button>
+  return (
+    <div className="flex px-4 sm:px-10 md:px-20 lg:px-40 xl:px-80 justify-center">
+      {/* <Button className="mx-1 light">Home</Button>
                 <Button className="mx-1">Archive</Button>
                 <Button className="mx-1">Submit</Button> */}
-            <Tabs defaultValue="account" className="w-full max-w-[400px]">
-                <TabsList>
-                    <TabsTrigger value="home"><Link href="/">home</Link></TabsTrigger>
-                    <TabsTrigger value="archive"><Link href="/archive">archive</Link></TabsTrigger>
-                    <TabsTrigger value="submit"><Link href="/submit">submit</Link></TabsTrigger>
-                </TabsList>
-                {/* <TabsContent value="account">Make changes to your account here.</TabsContent>
+      <Tabs defaultValue="account" className="max-w-[400px]">
+        <TabsList>
+          <TabsTrigger value="home">
+            <Link href="/">home</Link>
+          </TabsTrigger>
+          <TabsTrigger value="archive">
+            <Link href="/archive">archive</Link>
+          </TabsTrigger>
+          <TabsTrigger value="submit">
+            <Link href="/submit">submit</Link>
+          </TabsTrigger>
+        </TabsList>
+        {/* <TabsContent value="account">Make changes to your account here.</TabsContent>
                 <TabsContent value="password">Change your password here.</TabsContent> */}
-            </Tabs>
-        </div>
-    )
+      </Tabs>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,51 @@
-import Image from "next/image";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card"
+} from "@/components/ui/hover-card";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        palestine solidarity archive.
-      </h1>
       <br></br>
-      <p className="text-xl text-muted-foreground">
+      <p className="text-xl primary-foreground">
         The
         <HoverCard>
-          <HoverCardTrigger>&nbsp;Palestine Solidarity Archive&nbsp;</HoverCardTrigger>
+          <HoverCardTrigger>
+            &nbsp;Palestine Solidarity Archive&nbsp;
+          </HoverCardTrigger>
           <HoverCardContent>
             can put stuff here, trying something
           </HoverCardContent>
         </HoverCard>
-
-        aims to preserve and document the history and memory of Palestinians, the resistance movement, and global expressions of solidarity through art, stickers, graffiti, and protests. By doing so, we seek to preserve collective memories of these injustices and genocides, along with the unwavering acts of solidarity and resistance.
-        <br></br><br></br>
-        We are currently gathering original photographs of signs, art, stickers, and graffiti that support the Palestinian liberation movement, which you may encounter in public spaces such as streets, walls, subways, bridges, and protests.
-
-        Please keep in mind safety, security, and anonymity when submitting materials with identifying information, such as photos, email addresses, and names (made optional in the form). Digital security tools can help strip metadata and obscure faces, especially for recent protests.
-
-        <br></br><br></br>
-        Your submission may be featured in an online gallery or a physical installation. We will not be using any personal/identifying data when publishing works.
-        Submission deadline: July 27, 2024
-
-        <br></br><br></br>
-        If you have any questions, please contact: tapestry.gallery.nyc@gmail.com
+        aims to preserve and document the history and memory of Palestinians,
+        the resistance movement, and global expressions of solidarity through
+        art, stickers, graffiti, and protests. By doing so, we seek to preserve
+        collective memories of these injustices and genocides, along with the
+        unwavering acts of solidarity and resistance.
+        <br></br>
+        <br></br>
+        We are currently gathering original photographs of signs, art, stickers,
+        and graffiti that support the Palestinian liberation movement, which you
+        may encounter in public spaces such as streets, walls, subways, bridges,
+        and protests. Please keep in mind safety, security, and anonymity when
+        submitting materials with identifying information, such as photos, email
+        addresses, and names (made optional in the form). Digital security tools
+        can help strip metadata and obscure faces, especially for recent
+        protests.
+        <br></br>
+        <br></br>
+        Your submission may be featured in an online gallery or a physical
+        installation. We will not be using any personal/identifying data when
+        publishing works. Submission deadline: July 27, 2024
+        <br></br>
+        <br></br>
+        If you have any questions, please contact:
+        <Link href={`mailto:tapestry.gallery.nyc@gmail.com`}>
+          tapestry.gallery.nyc@gmail.com
+        </Link>
       </p>
-
-      {/* <img src="https://drive.google.com/uc?export=download&id=1BPVT40O8iShnVJ5Il5cfmtYvrg0j7iek"></img> */}
     </div>
   );
 }
