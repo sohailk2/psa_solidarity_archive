fc890ca2aab5063cc07772230a0db761983916bb
Syeda Tabassum <syedatabassum93@gmail.com>
2024-08-07 23:59:58 -0400

    add tags and date

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,24 +1,28 @@
 import Gallery from "./gallery";
 import {RetrievalObject} from "./gallery";
 
-export default async function Archive() {    
-    const response = await fetch('https://psa-solidarity-archive.vercel.app/api/archive/', {method: 'POST', next: { revalidate: 60 }})
-    // const response = await fetch('http://localhost:3000/api/archive', {next: { revalidate: 10 }})
+export default async function Archive() {
+  const response = await fetch(
+    "https://psa-solidarity-archive.vercel.app/api/archive/",
+    { method: "POST", next: { revalidate: 60 } }
+  );
+  // const response = await fetch("http://localhost:3000/api/archive", {
+  //   method: "POST",
+  //   next: { revalidate: 10 },
+  // });
 
-    // TODO: figure this out -> vercel thing with backend and client deployed at same time
-    // deploy next js backend route seperately vercel, deploy it twice?
-    let works: RetrievalObject[] = []
-    try {
-        const res = await response.json()
-        works = res.data
-    } catch {
-        console.log("api/archive not found")
-    }
-    console.log("Works", works)
+  // TODO: figure this out -> vercel thing with backend and client deployed at same time
+  // deploy next js backend route seperately vercel, deploy it twice?
+  let works: RetrievalObject[] = [];
+  try {
+    const res = await response.json();
+    works = res.data;
+  } catch {
+    console.log("api/archive not found");
+  }
+  console.log("Works", works);
 
-    // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#on-demand-revalidation
-    // its caching the api responses. we don't want that
-    return (
-        <Gallery works={works}/>
-    )
+  // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#on-demand-revalidation
+  // its caching the api responses. we don't want that
+  return <Gallery works={works} />;
 }
--- src/app/components/navigation/navbar.tsx
+++ src/app/components/navigation/navbar.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { link } from "fs";
 import Link from "next/link";
+
+
 
 interface NavBarLink {
   linkText: string;
@@ -9,31 +8,32 @@ interface NavBarLink {
   target?: string;
 }
 
-
 const navBarLinks = [
   { linkText: "about", href: "/" },
   { linkText: "archive", href: "/archive" },
   { linkText: "submit", href: "https://forms.gle/HmCyG4P1Ytbg9uSG9", target: "_blank" }
-]
-const styledLink = (navBarLink: NavBarLink) => {
+];
+
+const StyledLink = ({ navBarLink }: { navBarLink: NavBarLink }) => {
   return (
-    <li className="c-nav__item px-2">
+    <li key={navBarLink.href} className="c-nav__item px-2">
       <Link href={navBarLink.href} target={navBarLink.target}>
         <strong>
           {navBarLink.linkText}
-          <div className="fadingEffect" ></div>
+          <div className="fadingEffect"></div>
         </strong>
       </Link>
     </li>
-  )
-}
+  );
+};
 
 export default function NavBar() {
-
   return (
     <div className="flex flex-row px-4 sm:px-10 md:px-20 lg:px-40 xl:px-80 justify-center">
       <ul className="c-nav flex flex-row">
-        {navBarLinks.map((link) => styledLink(link))}
+        {navBarLinks.map((link) => (
+          <StyledLink key={link.href} navBarLink={link} />
+        ))}
       </ul>
     </div>
   );
