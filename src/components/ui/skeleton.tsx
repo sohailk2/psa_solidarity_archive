794f7b66538aaf131e389f514d7e242a1d721086
Sohail Khan <sohailk2@illinois.edu>
2024-08-11 11:20:22 -0400

    Spacing Updates

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -7,6 +7,7 @@ function Skeleton({
   return (
     <div
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      style={{display: 'hidden', backgroundImage: 'https://media.istockphoto.com/id/827303250/photo/yellow-crayon-doodles-background-texture.jpg?s=612x612&w=0&k=20&c=7BBue-aeN9kG962xRh0r0R6sGYc2dmPY72lcjSZzQEM='}}
       {...props}
     />
   )
